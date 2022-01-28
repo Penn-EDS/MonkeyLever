e3554d4825415fc4459bad55fbaebd4a91448a87
quijanop <93285324+quijanop@users.noreply.github.com>
2022-01-28 12:32:55 -0500

    Add files via upload

--- touchLeverV1.1/printToOled.ino
+++ touchLeverV1.1/printToOled.ino
@@ -1,18 +1,23 @@
 //Uses the SSD1306 Oled
 
-void printToOled(int TValue,int dividerBitVoltage, int resValue){
+void printToOled(int TValue, char dividerBitVoltage[5]){
 
     display.clearDisplay();
-    display.setTextSize(2);
+    display.setTextSize(1);
     display.setTextColor(WHITE);
     display.setCursor(0,0);
+    display.print("Press enter to set");
+    display.setCursor(0,9);
+    display.print("TVAL");
+    display.setCursor(0,20);
+    display.setTextSize(2);
     display.print("TVal:");
     display.print(TValue);
-    display.setCursor(0,20);
-    display.print("R:");
-    display.print(resValue);
-    display.setCursor(100,20);
-    display.print("kO");
+    //display.setCursor(0,20);
+    //display.print("R:");
+    //display.print(resValue);
+    //display.setCursor(100,20);
+    //display.print("%");
     display.setCursor(0,40);  
     display.print("Div:");
     display.print(dividerBitVoltage);  
--- touchLeverV1.1/touchLeverV1.1.ino
+++ touchLeverV1.1/touchLeverV1.1.ino
@@ -10,10 +10,10 @@
   ***********Oled Setup*********
   ******************************/
 
+#include <string.h>
 #include <stdbool.h>
 #include <SPI.h>
 #include <Wire.h>
-#include <Bounce2.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -36,170 +36,187 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
   ******************************/
 
 #define BUTTON_PIN_1 2              //Setting pins D2 and D3 as buttons
-#define BUTTON_PIN_2 3                    
+#define BUTTON_PIN_2 3              
 
-const int source = A1;              //Analog reading of the baseResistance at the source side.
+char volDiv[2][6];                  //Strings to show for A0 and Threshold Value comparison
+
 const int vo2 = A2;                 //Analog reading of the baseResistance at the potentiometer side. Used to calculate current.
 const int touchLever = A0;          //Select analog pin for the lever
-const int thresholdValueInput = A3; //This analog pins reads the voltage divider of a potentiometer to act as a knob.
 const int ledPin = 13;              //Select the pin for the LED
 const int output = 6;               //Digital output for the device
 
-int thresholdValue = 1012;           //Threshold Voltage to detect touch. Input by the user. Default to 800
+int thresholdValue = 750;           //Threshold Voltage to detect touch. Input by the user. Default to 153 or 3 Volts.
 int sensorValue = 0;                //Variable to store the value coming from the touchLeverPin.
-int sourceBitValue = 0;             //Variables to store analog readings
 int vo2BitValue = 0;
 int touchLeverBitValue = 0;
 
 int enter = HIGH;                   //Variables to hold button state
 int up = HIGH;
-int debTime = 50;                    //Debounce time
 int ones = 0;
 int tens = 0;
 int hundreds = 0;
 int thousands = 0;
 
-float baseResistance = 776.6;       //The units are kΩ. This resistance prevents the potentiometer that leads to make a short circuit, it also 
+int potPercent[11] = {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+int potPerVal = 0;
+
+float baseResistance = 1000;        //The units are kΩ. This resistance prevents the potentiometer that leads to make a short circuit, it also 
                                     //raises resistance floor. It will also be used to measure the resistance of the potentiometer. 
                                     //Its value is determined beforehand.
 float potentiometer = 0.0;          //Variable to store potentiometer resistance.
-float topVoltDiv = 0.0;             //Variable to sum both pot and base resistance to show on the display.
 
-bool tvalSet = false;                //flag to see if TVal has been set
+bool tvalSet = false;               //flag to see if TVal has been set
 
-int unit = 1;                      //Variable to cycle through units
-
-Bounce debEnter = Bounce();         //Bounce object for the buttons
-Bounce debUp = Bounce(); 
+int unit = 1;                       //Variable to cycle through units
 
 void setup() {
   // put your setup code here, to run once:
-  delay(200); //Let Arduino start
+  delay(1000); //Let Arduino start
+
+  strcpy(volDiv[0], "Above");       //Strings for Voltage Divider and Touch Lever comparison
+  strcpy(volDiv[1], "Below");
 
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
   display.clearDisplay();
 
-  pinMode(BUTTON_PIN_1,INPUT_PULLUP);   // Setup the first button with an internal pull-up :
-  debEnter.attach(BUTTON_PIN_1);         // After setting up the button, setup the Bounce instance :
-  debEnter.interval(debTime);                  // interval in ms
-     
-  pinMode(BUTTON_PIN_2,INPUT_PULLUP);   // Setup the second button with an internal pull-up :
-  debUp.attach(BUTTON_PIN_2);            // After setting up the button, setup the Bounce instance :
-  debUp.interval(debTime);                     // interval in ms
+  pinMode(BUTTON_PIN_1,INPUT_PULLUP);   // Setup the first button with an internal pull-up
+  pinMode(BUTTON_PIN_2,INPUT_PULLUP);   // Setup the second button with an internal pull-up 
 
   pinMode(ledPin, OUTPUT);
   pinMode(output, OUTPUT);
-  
+
   Serial.begin(9600);
   Serial.println("Begin");
 
-  ones = thresholdValue / 10;
-  ones = thresholdValue * 10;
-  ones = thresholdValue - ones;
-  tens = thresholdValue / 100;
-  tens = thresholdValue * 100;
-  tens = thresholdValue - tens;
+  enter = HIGH;
+  tvalSet = HIGH;
 
-  while(not tvalSet){
-    displayTvalSet(thresholdValue);  
-    debEnter.update();  // Update the Bounce instances
-    debUp.update();
-
-    enter = debEnter.read();  // Get the updated value for buttons
-    up = debUp.read();
-    
-    //Serial.println(ones);
-    //Serial.println(tens);
-    //Serial.println(hundreds);
-    //Serial.println("**************");  
-
-
-      ones = thresholdValue / 10;
-      Serial.println(ones);
-      ones = ones * 10;
-      Serial.println(ones);
-      ones = thresholdValue - ones;
-      Serial.println(ones);
-      Serial.println("**************");
-      tens = thresholdValue / 100;
-      tens = tens * 100;
-      tens = thresholdValue - tens;
-      hundreds = thresholdValue / 1000;
-      hundreds = hundreds * 1000;
-      hundreds = thresholdValue - hundreds;  
-    if (enter == LOW){
-      unit += 1;
-      if (unit >= 5){
-        tvalSet = true;
-      }
-    }
-
-    if (up == LOW){          
-      switch(unit){
-        case 1:
-          if ((ones) < 9){
-            thresholdValue += 1;
-          }
-          else{
-            thresholdValue -= 9;
-          }
-          break;
-        case 2:
-          if ((hundreds) < 90){
-            thresholdValue += 10;
-          }
-          else{
-            thresholdValue -= 90;
-          }
-          break;
-        case 3:
-          if (thousands < 900){
-            thresholdValue += 100;
-          }
-          else{
-            thresholdValue -= 900;
-          }
-          break;
-        case 4:
-          if ((thresholdValue) < 1000){
-            thresholdValue += 1000;
-          }
-          else{
-            thresholdValue -= 1000;
-          }
-          break;
-      }
-    }
-  }
+  delay(100);   //At the moment this makes the program not run the threshold selection on startup
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  //thresholdValue = analogRead(thresholdValueInput);  //Read values from the analog inputs
-  sourceBitValue = analogRead(source);
-  vo2BitValue = analogRead(vo2);
+  enter = digitalRead(BUTTON_PIN_1);
+  
+  if (enter == LOW){  //If enter button is pressed at any time in the program, threshold value selection starts
+    delay(200);
+    
+    tvalSet = false;    //Resets conditions to exit loop
+    unit = 1;
+    
+    while(not tvalSet){
+      
+      displayTvalSet(thresholdValue);
+
+      enter = digitalRead(BUTTON_PIN_1);
+      up = digitalRead(BUTTON_PIN_2);
+    
+      //Serial.println(ones);
+      //Serial.println(tens);
+      //Serial.println(hundreds);
+      //Serial.println("**************");  
+        ones = thresholdValue / 10;
+        //Serial.println(ones);
+        ones = ones * 10;
+        //Serial.println(ones);
+        ones = thresholdValue - ones;
+        //Serial.println(ones);
+        //Serial.println("**************");
+        tens = thresholdValue / 100;
+        tens = tens * 100;
+        tens = thresholdValue - tens;
+        hundreds = thresholdValue / 1000;
+        Serial.println(hundreds);
+        hundreds = hundreds * 1000;
+        Serial.println(hundreds);
+        hundreds = thresholdValue - hundreds;
+        Serial.println(hundreds);  
+        Serial.println("**************");
+        
+      if (enter == LOW){  //If enter is pressed, got to next unit
+        delay(200);
+        
+        unit += 1;
+        
+        if (unit >= 5){   //If all units have been set, set variable to end threshold value selection
+          tvalSet = true;
+        }
+      }
+
+      if (up == LOW){     //Cycle through numbers 0 to 9 (in most cases)
+        delay(200);
+        
+        switch(unit){
+          case 1:
+            if ((ones) < 9){
+            thresholdValue += 1;
+            }
+            else{
+              thresholdValue -= 9;
+            }
+            break;
+          case 2:
+            if ((tens) < 90){
+              thresholdValue += 10;
+            }
+            else{
+              thresholdValue -= 90;
+            }
+            break;
+          case 3:
+            if (hundreds < 900){
+              thresholdValue += 100;
+            }
+            else{
+              thresholdValue -= 900;
+            }
+            break;
+          case 4:
+            if ((thresholdValue) < 1000){
+              thresholdValue += 1000;
+            }
+            else{
+              thresholdValue -= 1000;
+            }
+            break;
+        }
+      }
+    } 
+    if (thresholdValue > 1023){  //Does not let threshold value be greater than the 10-bit value the ADC reads
+      thresholdValue = 1023;
+    }
+  }
+  
+  vo2BitValue = analogRead(vo2);  //Read values from the analog inputs
   touchLeverBitValue = analogRead(touchLever);
 
-  potentiometer = potReading(sourceBitValue,vo2BitValue,touchLeverBitValue); //Get current resistance value of the potentiometer.
-
-  topVoltDiv = baseResistance + potentiometer;
-
-  if (touchLeverBitValue <951){
-    printToOled(thresholdValue, touchLeverBitValue, topVoltDiv);  //update display
+  potentiometer = potReading(1023,vo2BitValue,touchLeverBitValue); //Get current resistance value of the potentiometer.
+  potentiometer *= 2.5; //Calibration constant
+  
+  Serial.print("Potentiometer in kohms:");
+  Serial.println(potentiometer);
+  
+  
+    potPerVal = (potentiometer) / 450;  //Divides the potentiometer readings in 
+  
+  if (potPerVal > 10){
+    potPerVal = 10;
   }
-  else{
-    printToOled(thresholdValue, touchLeverBitValue, -1);  //update display
+  else if (potPerVal < 0){
+    potPerVal = 0;
   }
   
   if (touchLeverBitValue > thresholdValue){
+    printToOled(thresholdValue, volDiv[0]);  //update display
     digitalWrite(ledPin, LOW); 
     digitalWrite(output, LOW); //When the lever is not touched, the output is low
   }
   else{
-  digitalWrite(ledPin, HIGH);
-  digitalWrite(output, HIGH);  //When the lever is touched, the output is high
+    printToOled(thresholdValue, volDiv[1]);  //update display
+    digitalWrite(ledPin, HIGH);
+    digitalWrite(output, HIGH);  //When the lever is touched, the output is high
   }
   
-  delay(25);
+  delay(10);
 }
